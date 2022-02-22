8197632ad0911f9b5cbf24cef389dc2840039f39
Expired <imoses2@hotmail.com>
2022-02-22 02:43:10 -0700

    fixes #10 - post api notes route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db.json');
+const { v4: uuidv4 } = require('uuid');
+const { createNewNote, validateNote } = require('../../lib/notes');
 
 router.get('/notes', (request, response) => {
 	response.json(notes);
@@ -8,6 +10,15 @@ router.get('/notes', (request, response) => {
 router.post('/notes', (request, response) => {
 	//do something
 	console.log('api post request made');
+	request.body.id = uuidv4(); 
+	
+	if(!validateNote(request.body)) {
+		response.status(400).send('The note is not properly formatted.');
+		return;
+	}
+	
+	const note = createNewNote(request.body, notes);
+	response.json(note);
 });
 
 module.exports = router;
